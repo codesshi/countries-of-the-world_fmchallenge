382196a05df5b22da0e05be49006e6d7e88946f4
Fabrizio Giannobili <codesshi@gmail.com>
2021-07-11 16:52:02 +0200

    [MISC] Install and configure Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "stories": ['../src/components/**/*.stories.js','../src/screens/**/*.stories.js'],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
