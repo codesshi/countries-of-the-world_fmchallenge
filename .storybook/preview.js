9a9df40636bc0cbe3990e41d49eca92292ba1ed7
Fabrizio Giannobili <codesshi@gmail.com>
2021-07-11 17:00:24 +0200

    [MISC] Add ThemeProvider as Global Decorator to Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import '../src/index.css';
+import {ThemeProvider} from '../src/hooks/useTheme';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,3 +11,26 @@ export const parameters = {
   },
   layout: "fullscreen"
 }
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      // Array of plain string values or MenuItem shape (see below)
+      items: ['light', 'dark'],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+    },
+  },
+};
+
+const themeDecorator = (Story, context) => {
+  const theme = context.globals.theme
+
+  return <ThemeProvider defaultTheme={theme}><Story {...context}/></ThemeProvider>
+}
+
+export const decorators = [themeDecorator]
