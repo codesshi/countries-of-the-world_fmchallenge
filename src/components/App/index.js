36fd432f9568923153ed8b2611d093147150f721
Fabrizio Giannobili <codesshi@gmail.com>
2021-08-13 18:46:19 +0200

    [FEAT] Set basic skeleton of App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,18 @@
-import logo from '../../assets/logo.svg';
-import './App.css';
+import "./App.css"
+import { Switch, Route } from "react-router-dom"
+import { ThemeProvider } from '../../hooks/useTheme'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <ThemeProvider>
+            <div className="App">
+                <Switch>
+                    <Route exact path="/">
+                    </Route>
+                    <Route exact path="/countries/:code">
+                    </Route>
+                </Switch>
+            </div>
+        </ThemeProvider>
+    );
 }
-
-export default App;
