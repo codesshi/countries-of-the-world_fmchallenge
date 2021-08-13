45d159554f5d06546d835f5c33f205aa18e09e46
Fabrizio Giannobili <codesshi@gmail.com>
2021-08-13 19:50:22 +0200

    [REFACTOR] Remove screens folder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "stories": ['../src/components/**/*.stories.js','../src/screens/**/*.stories.js', '../src/sections/**/*.stories.js'],
+  "stories": ['../src/components/**/*.stories.js','../src/sections/**/*.stories.js'],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
