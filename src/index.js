af0ce0692d432ab34d08527a3a25075a657861af
Fabrizio Giannobili <codesshi@gmail.com>
2021-08-16 18:47:48 +0200

    [FIX] Add basename to Router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/countries-of-the-world_fmchallenge">
       <App />
     </Router>
   </React.StrictMode>,
