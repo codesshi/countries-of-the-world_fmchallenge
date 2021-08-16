ffc8d69796370d8d8e5ed189a509c21c900e14d3
Fabrizio Giannobili <codesshi@gmail.com>
2021-08-16 19:51:39 +0200

    [REFACTOR] Move basename declaration in .env file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/countries-of-the-world_fmchallenge">
+    <Router basename={process.env.REACT_APP_BASENAME}>
       <App />
     </Router>
   </React.StrictMode>,
